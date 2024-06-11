b5e7da8e72adf790325a3b18bc419196ae149efb
golemworks <korotkih.vladimir.isr21.1@gmail.com>
2024-06-11 23:12:40 +0200

    Вывод картинок из БД

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 initSqlJs({ locateFile: filename => `https://cdnjs.cloudflare.com/ajax/libs/sql.js/1.5.0/${filename}` })
     .then(SQL => {
         let db = new SQL.Database(); // Создаём виртуальную базу данных
-        db.exec("CREATE TABLE test (Name, Path); INSERT INTO test VALUES ('Mask', 'Mask.jpg'), ('Snorkel','Snorkel.jpg');"); // Заполняем таблицу тестовыми данными
+        db.exec("CREATE TABLE test (Name, Path); INSERT INTO test VALUES ('Mask', 'Mask.jpg'), ('Snorkel','Snorkel.jpg'), ('stratos','stratos.jpg'), ('Suunto','Suunto.jpg');"); // Заполняем таблицу тестовыми данными
         let stmt = db.prepare("SELECT * FROM test"); 
-        
-        let parent = document.querySelector('#parent');
+
+        const arr = [];
 
         while (stmt.step()){
-            let p = document.createElement('p');
-            p.textContent = stmt.get(); 
-            parent.appendChild(p);
+            arr.push(stmt.get()); 
         } 
-        stmt.free(); // Освобождаем ресурсы после выполнения запроса
+        //stmt.free(); // Освобождаем ресурсы после выполнения запроса
+
+         const container = document.getElementById('image-container');
+        
+         for (let i = 0; i < arr.length; i += 1) {  
+                const img = document.createElement('img');       
+                img.src = arr[i][1];
+                container.appendChild(img);
+          }
     });
+
+      
